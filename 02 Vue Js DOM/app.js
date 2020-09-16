5c934a4aa3e77dc931d269aa499905ea641b3030
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-16 18:53:01 +0530

    Vue Js DOM - Reactive properties and short hand syntaxes

--- 02 Vue Js DOM/app.js
+++ 02 Vue Js DOM/app.js
@@ -6,8 +6,21 @@ new Vue({
     link: "https://www.google.com",
     linkHTML: `<a href="https://www.linkedin.com">Linkedin</a>`,
     counter: 0,
+    counter2: 0,
     x: 0,
     y: 0,
+  },
+  computed: {
+    output() {
+      return this.counter > 5 ? "Greater than 5" : "Less than 5";
+    },
+  },
+  watch: {
+    counter: function (value) {
+      setTimeout(() => {
+        this.counter = 0;
+      }, 2000);
+    },
   },
   methods: {
     displayTitle() {
