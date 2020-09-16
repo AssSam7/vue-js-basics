ce0e2c147e15f7807233a6b6b39d7883b7d46a78
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-16 19:13:18 +0530

    Vue Js DOM - Dynamically adding CSS classes

--- 02 Vue Js DOM/app.js
+++ 02 Vue Js DOM/app.js
@@ -1,45 +1,45 @@
 new Vue({
   el: "#app",
   data: {
-    name: "Aslam MD",
-    title: "Hello",
-    link: "https://www.google.com",
-    linkHTML: `<a href="https://www.linkedin.com">Linkedin</a>`,
-    counter: 0,
-    counter2: 0,
-    x: 0,
-    y: 0,
+    // name: "Aslam MD",
+    // title: "Hello",
+    // link: "https://www.google.com",
+    // linkHTML: `<a href="https://www.linkedin.com">Linkedin</a>`,
+    // counter: 0,
+    // counter2: 0,
+    // x: 0,
+    // y: 0,
+    attachRed: false,
+    attachGreen: false,
+    attachBlue: false,
   },
-  computed: {
-    output() {
-      return this.counter > 5 ? "Greater than 5" : "Less than 5";
-    },
-  },
-  watch: {
-    counter: function (value) {
-      setTimeout(() => {
-        this.counter = 0;
-      }, 2000);
-    },
-  },
+  // computed: {
+  //   output() {
+  //     return this.counter > 5 ? "Greater than 5" : "Less than 5";
+  //   },
+  // },
+  // watch: {
+  //   counter: function (value) {
+  //     setTimeout(() => {
+  //       this.counter = 0;
+  //     }, 2000);
+  //   },
+  // },
   methods: {
-    displayTitle() {
-      this.title = "Hello World";
-      return `${this.title}`;
-    },
-
-    incrementCounter(step, e) {
-      this.counter += step;
-      console.log(e.target);
-    },
-
-    updateCoords(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-
-    alert() {
-      alert("Hello World!");
-    },
+    // displayTitle() {
+    //   this.title = "Hello World";
+    //   return `${this.title}`;
+    // },
+    // incrementCounter(step, e) {
+    //   this.counter += step;
+    //   console.log(e.target);
+    // },
+    // updateCoords(event) {
+    //   this.x = event.clientX;
+    //   this.y = event.clientY;
+    // },
+    // alert() {
+    //   alert("Hello World!");
+    // },
   },
 });
