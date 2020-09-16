a09ca7fee7824897c1478f9de83ba907bb8ba1a6
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-16 16:49:53 +0530

    02 Vue Js DOM - Using the v-html directive to output the raw HTML

--- 02 Vue Js DOM/app.js
+++ 02 Vue Js DOM/app.js
@@ -4,6 +4,7 @@ new Vue({
     name: "Aslam Mohammed",
     title: "Hello",
     link: "https://www.google.com",
+    linkHTML: `<a href="https://www.linkedin.com">Linkedin</a>`,
   },
   methods: {
     displayTitle() {
