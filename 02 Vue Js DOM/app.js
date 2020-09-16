75d8752e079597a0155a04fc602ca7ac2a93d915
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-09-16 17:41:25 +0530

    Vue Js DOM - Handling the events

--- 02 Vue Js DOM/app.js
+++ 02 Vue Js DOM/app.js
@@ -5,11 +5,23 @@ new Vue({
     title: "Hello",
     link: "https://www.google.com",
     linkHTML: `<a href="https://www.linkedin.com">Linkedin</a>`,
+    counter: 0,
+    x: 0,
+    y: 0,
   },
   methods: {
     displayTitle() {
       this.title = "Hello World";
       return `${this.title}`;
+    },
+
+    incrementCounter() {
+      this.counter++;
+    },
+
+    updateCoords(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
     },
   },
 });
