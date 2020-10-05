a2145817fea06e3a28acb48f1b6c21fd8bd35787
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-10-06 00:31:38 +0530

    Adding Section 9 - Advanced Components Usage

---
+++ 09 Advanced Components/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h1>Aslams Musings</h1>
+          <p>Parent to children</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from "./components/Quote.vue";
+
+export default {
+  components: {
+    appQuote: Quote,
+  },
+};
+</script>
+
+<style></style>
---
+++ 09 Advanced Components/src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+div {
+  margin: 3rem auto;
+  padding: 3rem;
+  border: 1px solid crimson;
+  border-radius: 10px;
+  box-shadow: 1px 2px 6px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+</style>
