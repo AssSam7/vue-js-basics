c748b20324ab2e3a007ebf8e8483a970436a88d1
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2020-10-13 16:57:39 +0530

    11 Handling Forms - Submitting the form

--- 11 Handling Forms/src/App.vue
+++ 11 Handling Forms/src/App.vue
@@ -118,12 +118,14 @@
         <div
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3"
         >
-          <button class="btn btn-primary">Submit!</button>
+          <button class="btn btn-primary" @click.prevent="submitForm">
+            Submit!
+          </button>
         </div>
       </div>
     </form>
     <hr />
-    <div class="row">
+    <div class="row" v-if="isSubmitted">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
@@ -165,7 +167,13 @@ export default {
       selectedPriority: "High",
       priorities: ["High", "medium", "low"],
       dataSwitch: false,
+      isSubmitted: false,
     };
+  },
+  methods: {
+    submitForm() {
+      this.isSubmitted = true;
+    },
   },
   components: {
     appSwitch: Switch,
